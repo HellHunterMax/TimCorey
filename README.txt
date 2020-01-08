7257c8b60a3b433e74658707ce8ea1050e2952dc
Max <hellhunter15@hotmail.com>
2020-01-08 05:28:23 +0100

    APP NOW READS THE TEXTFILE AND ADDS USERS
    added a Method that reads the file and then closes it.
    then it adds users to the dataset.

--- README.txt
+++ README.txt
@@ -1,6 +1,8 @@
 ### Tim Corey exercises ###
 Tim Corey has a set of exercises on his website that i am planning to do and upload to Github for practice.
 
-1. TextFileChallengeStarterCode
+Link to Website: https://www.iamtimcorey.com/p/c-weekly-challenges
+
+1. TextFileChallengeStarterCode https://www.iamtimcorey.com/courses/c-weekly-challenges/lectures/6877618
 2.
 
--- TextFileChallengeStarterCode/TextFileChallenge/ChallengeForm.cs
+++ TextFileChallengeStarterCode/TextFileChallenge/ChallengeForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,12 +14,43 @@ namespace TextFileChallenge
     public partial class ChallengeForm : Form
     {
         BindingList<UserModel> users = new BindingList<UserModel>();
+        public static string DataSet = @".\StandardDataSet.csv";
 
         public ChallengeForm()
         {
             InitializeComponent();
 
+            OpenFile();
+
             WireUpDropDown();
+        }
+
+        private void OpenFile()
+        {
+            //FirstName,LastName,Age,IsAlive
+            var file = new FileStream(DataSet, FileMode.Open);
+            var reader = new StreamReader(file);
+            List<string[]> Data = new List<string[]>();
+            string[] ListOrder = reader.ReadLine().Split(',');
+
+            while (!reader.EndOfStream)
+            {
+                Data.Add(reader.ReadLine().Split(','));
+            }
+            reader.Close();
+            foreach (string[] data in Data)
+            {
+                /*
+                 * bool check = true;
+                    Console.WriteLine(check ? "Checked" : "Not checked");  // output: Checked
+
+                    Console.WriteLine(false ? "Checked" : "Not checked");  // output: Not checked
+                */
+                UserModel m = new UserModel() { FirstName = data[0], LastName = data[1], Age = int.Parse(data[2]), IsAlive = (int.Parse(data[3]) == 1)};
+                users.Add(m);
+            }
+
+
         }
 
         private void WireUpDropDown()
